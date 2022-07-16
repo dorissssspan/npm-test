835727f9ba17d400c4f7cc51232659d286dc0b25
dorissssspan <962752497@qq.com>
2022-07-16 16:25:17 +0800

    初始化提交

---
+++ server.js
@@ -0,0 +1,8 @@
+// console.log('hello lodash');
+
+// const lodash = require('lodash');
+// console.log(lodash.random(1,10));
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
